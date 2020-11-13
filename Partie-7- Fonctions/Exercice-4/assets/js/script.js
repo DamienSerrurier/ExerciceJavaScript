fc2005cd8d66b92731fcfdad9e74a5ad6e52fb13
DamienSerrurier <you@example.comdamien.ser@laposte.netgit config --global user.name DamienSerruriergit config --global user.email you@example.comdamien.ser@laposte.net>
2020-11-13 14:02:31 +0100

    Ajout exercice 5 et 6

--- Partie-7- Fonctions/Exercice-4/assets/js/script.js
+++ Partie-7- Fonctions/Exercice-4/assets/js/script.js
@@ -1,3 +1,3 @@
 let imageManip = document.querySelector('img');
-imageManip.onclick = () => imageManip.style.width = "70rem";
-imageManip.ondblclick = () => imageManip.style.width = "50rem";
+imageManip.onclick = () => {imageManip.style.width = "70rem"};
+imageManip.onmouseout = () => {imageManip.style.width = ""};
---
+++ Partie-7- Fonctions/Exercice-6/assets/js/script.js
@@ -0,0 +1,13 @@
+
+//Fonction fléché
+/*(name) => {
+    `Bonjour, ${name} ! Comment vas-tu ?`;
+}*/
+
+//Fonction nomé
+function textName(name) {
+    return 'Bonjour, ' + name + ' ! Comment vas-tu ?';
+ }
+ 
+console.log(document.getElementById('name').textContent);
+alert(document.getElementById('name').textContent);
